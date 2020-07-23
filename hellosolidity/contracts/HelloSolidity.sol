6a8d26a51f59f8dc1b3c6662beb976362b3d424c
Siwakorn Srisakaokul <siwakorns@sec.or.th>
2020-07-23 14:26:21 +0700

    ลอง Deploy HelloSolidity แล้วใช้งาน Mist

--- hellosolidity/contracts/HelloSolidity.sol
+++ hellosolidity/contracts/HelloSolidity.sol
@@ -1,1 +1,32 @@
 pragma solidity >=0.5.0;
+// comment
+/* multi-line comment */
+
+contract HelloSolidity {
+
+    // state variable
+    uint public myValue;
+    constructor() public {
+        myValue = 5;
+    }
+
+    function increase() public { // Transactional call: มีการอัพเดต state variable
+        myValue++;
+    }
+
+    function decrease() public {
+        myValue--;
+    }
+
+    function setValue(uint newValue) public {
+        myValue = newValue;
+    }
+
+    function getValue() public returns (uint) { // Non-transactional call
+        return myValue;
+    }
+
+    function myFunc(uint a, uint b) public pure returns (uint, uint) { // Pure function: ไม่ได้ยุ่งเกี่ยวกับ state variable, คำนวณอย่างเดียว
+        return (a * b, a + b);
+    }
+}
